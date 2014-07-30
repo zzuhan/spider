5c02e03b0b3ebcc71b733421924489bceaf1921f
cwt <646766469@qq.com>
2014-07-31 00:32:39 +0800

    Update fs.js
    ff

--- modules/fs.js
+++ modules/fs.js
@@ -2,11 +2,8 @@ var fs = require('fs');
 exports.editFile = function(key,content){
 	var id = key;
 	fs.readFile(__dirname + '/data.json', function (err, data) {
-			data = JSON.parse(data);
-			if(data[0].files[id]){
-				data[0].files[id] = content;
-				fs.writeFile(__dirname + '/data.json',JSON.stringify(data));
-			}else{
-			}
-		});
+		data = JSON.parse(data);
+		data.files[id] = content;
+		fs.writeFile(__dirname + '/data.json',JSON.stringify(data));
+	});
 };
